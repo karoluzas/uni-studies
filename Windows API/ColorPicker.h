3317b4e16ef272bbc93745b847b35a73195db587
karoluzas <79944650+karoluzas@users.noreply.github.com>
2023-11-27 21:34:44 +0200

    working windows api paint program

---
+++ Windows API/ColorPicker.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <windows.h>
+
+extern "C" __declspec(dllimport) int WINAPI AnotherWindow(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow, HWND hMainWindow);
